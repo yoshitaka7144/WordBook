dcb94828a23bd7410d26cd278b4a5cbd1c13c552
yoshitaka7144 <yoshitaka7144@gmail.com>
2021-07-30 00:43:44 +0900

    レンタルサーバー関連の定数定義。viewportの文を追加。

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Word Book</title>
     <link rel="icon" href="image/favicon_mikan.ico">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
